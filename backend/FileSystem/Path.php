2cbe7a8abaafe28d1c28ed1bdb298f12c6b04d59
plencka <paulina.lencka@blumilk.pl>
2021-07-12 09:52:52 +0200

    Applied ecs.

--- backend/FileSystem/Path.php
+++ backend/FileSystem/Path.php
@@ -1,22 +1,24 @@
 <?php
 
+declare(strict_types=1);
 
 namespace Internships\FileSystem;
 
-
 class Path
 {
-    public function __construct(protected string $root)
-    {
+    public function __construct(
+        protected string $root
+    ) {
     }
 
-    protected function normalize(string $path) : string
+    public function getFull($relativePath): string
     {
-        return preg_replace('#/+#','/',$path);
-    }
-
-    public function getFull($relativePath) : string{
         $fullPath = $this->root . "/" . $relativePath . "/";
         return $this->normalize($fullPath);
+    }
+
+    protected function normalize(string $path): string
+    {
+        return preg_replace("#/+#", "/", $path);
     }
 }
