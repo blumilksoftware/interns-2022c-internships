45a08087b0b3258e438dd59871c2c4652a077a3b
brzozka <bartosz.brzozka@blumilk.pl>
2021-07-12 13:32:00 +0200

    #28 Initial setup for landing page
    Include installing tailwind and dependencies

---
+++ frontend/views/Landing.vue
@@ -0,0 +1,4 @@
+<template>
+  <div id="landing"></div>
+</template>
+<script></script>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
