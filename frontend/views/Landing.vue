4e58fb4c705475b64c18238a7aeebe08420ffb0a
brzozka <bartosz.brzozka@blumilk.pl>
2021-07-15 13:39:48 +0200

    #28 Added polygon on second image
    As in title.

--- frontend/views/Landing.vue
+++ frontend/views/Landing.vue
@@ -176,6 +176,26 @@
         lg:absolute lg:left-0 lg:h-full lg:w-1/2
       "
     >
+      <svg
+        class="
+          hidden
+          lg:block
+          absolute
+          right-0
+          inset-y-0
+          h-full
+          w-48
+          text-white
+          transform
+          translate-x-1/2
+        "
+        fill="currentColor"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="none"
+        aria-hidden="true"
+      >
+        <polygon points="50,0 100,0 50,100 0,100" />
+      </svg>
       <img
         class="w-full h-full object-cover"
         src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
