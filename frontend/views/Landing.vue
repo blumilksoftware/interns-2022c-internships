9c570cf9acff612a6db9c81a7efeddb3cb8d6aee
brzozka <39692087+brzozka@users.noreply.github.com>
2021-07-22 15:18:09 +0200

    #55 - mobile img fix (#62)
    Mobile version images seemd overwhelming so we are hiding one untill the resolution is at least 1024px

--- frontend/views/Landing.vue
+++ frontend/views/Landing.vue
@@ -158,7 +158,7 @@
         </main>
       </div>
     </div>
-    <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+    <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
       <img
         class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
         src="https://images.unsplash.com/photo-1566647387313-9fda80664848?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2602&q=80"
