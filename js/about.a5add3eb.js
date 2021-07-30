c1c370d13b087111b73ce6d82de7805705cc51a1
krzysztofrewak <krzysztof.rewak@gmail.com>
2021-07-30 06:50:36 +0000

    Deploying to public from @ blumilksoftware/internships@6b44c28cc090c90f183e9cc1c073505688c04019 🚀

--- js/about.a5add3eb.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"07c4":function(n,t,a){"use strict";a.r(t);var c=a("7a23"),e={class:"about"},o=Object(c["g"])("h1",null,"This is an about page",-1);function s(n,t){return Object(c["p"])(),Object(c["d"])("div",e,[o])}const u={};u.render=s;t["default"]=u}}]);
-//# sourceMappingURL=about.a5add3eb.js.map
