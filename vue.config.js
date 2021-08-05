ef90f7914665eda7d02cea559842ff5b67946196
brzozka <brzozka108@gmail.com>
2021-08-05 15:12:31 +0200

    #103 Fixed vue.config.js
    After changing the location of index.html We needed to also indicate that in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,10 @@ module.exports = {
   chainWebpack: (config) => {
     config.entry("app").clear().add("./frontend/main.js").end();
     config.resolve.alias.set("@", path.join(__dirname, "./frontend"));
+    config.plugin("html").tap((args) => {
+      args[0].template = "./frontend/templates/index.html";
+      return args;
+    });
   },
   devServer: {
     port: 8080,
