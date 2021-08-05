c9fda68c7537c7aa995a5e77cb390f78cedfe197
plencka <paulina.lencka@blumilk.pl>
2021-08-05 10:52:52 +0200

    Changed api build path.

--- backend/Application/AppContainerDefinitions.php
+++ backend/Application/AppContainerDefinitions.php
@@ -7,6 +7,6 @@ use Internships\FileSystem\DirectoryManager;
 return [
     DirectoryManager::class => DI\autowire()
         ->constructorParameter("rootDirectoryPath", __DIR__ . "/../../")
-        ->constructorParameter("relativeApiPath", "/public/api/")
+        ->constructorParameter("relativeApiPath", "/dist/api/")
         ->constructorParameter("relativeResourcePath", "/resources/"),
 ];
