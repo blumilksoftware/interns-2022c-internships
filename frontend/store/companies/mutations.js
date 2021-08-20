5b8bda7c2310868f43ca1c25dda6f56ccc6827ae
brzozka <brzozka108@gmail.com>
2021-08-20 13:40:58 +0200

    #101 Changed import name
    As in title.

--- frontend/store/companies/mutations.js
+++ frontend/store/companies/mutations.js
@@ -1,29 +1,29 @@
-import types from "./mutation-types";
+import mutation from "./mutation-types";
 export default {
-  [types.SET_COMPANIES](state, companies) {
+  [mutation.SET_COMPANIES](state, companies) {
     state.companies = companies;
   },
-  [types.SET_FILTERS](state, filters) {
+  [mutation.SET_FILTERS](state, filters) {
     state.filters = filters;
   },
-  [types.SET_FACULTIES](state, faculties) {
+  [mutation.SET_FACULTIES](state, faculties) {
     state.faculties = faculties;
   },
-  [types.SET_DOCUMENTS](state, documents) {
+  [mutation.SET_DOCUMENTS](state, documents) {
     state.documents = documents;
   },
-  [types.SET_TAGS](state, tags) {
+  [mutation.SET_TAGS](state, tags) {
     state.tags = tags;
   },
-  [types.MANAGE_ACTIVE_TAGS](state, tag) {
+  [mutation.MANAGE_ACTIVE_TAGS](state, tag) {
     state.activeTags.includes(tag)
       ? state.activeTags.splice(state.activeTags.indexOf(tag), 1)
       : state.activeTags.push(tag);
   },
-  [types.RESET_ACTIVE_TAGS](state) {
+  [mutation.RESET_ACTIVE_TAGS](state) {
     state.activeTags = [];
   },
-  [types.SET_COMPANIES_MERGED](state, companiesMerged) {
+  [mutation.SET_COMPANIES_MERGED](state, companiesMerged) {
     state.companiesMerged = companiesMerged;
   },
 };
