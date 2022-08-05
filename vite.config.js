231d181572dbc86cbdeaf47ee679fc58cdbf219f
sbanasiak <92038757+sbanasiak@users.noreply.github.com>
2022-08-05 14:48:29 +0200

    #158 - Development environment fix (#160)
    * Fix vite alias
    
    * Removed unused file
    
    * Removed deploy.yml file
    
    * Update readme.md
    
    * Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default ({ mode }) => {
         },
         resolve: {
             alias: {
-                '@': './frontend',
+                '@': '/frontend',
             },
         },
         plugins: [
