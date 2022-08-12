e5db6430ca1ceebffe1ea2e56efcfa865640074a
plencka <lencka.paulina@gmail.com>
2022-08-12 12:20:19 +0200

    Moved App.vue to components

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,6 @@
 import "./bootstrap";
 import { createApp } from "vue";
-import App from "@/App.vue";
+import App from "@/components/App.vue";
 import router from "@/router/index.js";
 import "@/assets/tailwind.css";
 
