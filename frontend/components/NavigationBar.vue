0058ad573581187ba4e61e0157e4dd0547d84860
MatArasim <92038833+MatArasim@users.noreply.github.com>
2022-08-18 14:09:14 +0200

    #145 - Fully transition project from Pure CSS to TailwindUI (#168)
    * Removed all unnecessary page sections, changes to page design
    
    * Adjusting file name
    
    * Removed unused import
    
    * Revert vite config changes
    
    * Renamed icons
    
    * Update frontend/components/NavigationBar.vue
    
    Co-authored-by: Ewelina Lasowy <56546832+EwelinaLasowy@users.noreply.github.com>
    
    * Removing colors from tailwind.css
    
    * Some adjustments to company list
    
    * SearchBar naming fix
    
    * lint fix
    
    * Scollbar color change
    
    * All pure CSS removed or replaced with Tailwind UI
    
    * Frontend rework
    
    * Removed certain type of debugging
    
    * New page layout
    
    * Moved frontend config and lint to its working folder
    
    * Moved App.vue to components
    
    * Improved .env handling for Node and Vite
    
    * Changed .env handling for php
    
    * Updated .env.example
    
    * Fix for external dependency issue
    
    * Small fix to page sizing
    
    * TailWind UI Scaffold
    
    * Finished transition to tailwind
    
    * Removed unused lines
    
    * Updated github action
    
    * Moved mapbox style from .env to component
    
    * Update frontend/components/NavigationBar.vue
    
    Co-authored-by: Ewelina Lasowy <56546832+EwelinaLasowy@users.noreply.github.com>
    
    * Mapbox loading and resize fix
    
    * Adjustment to paga and code
    
    * Typo fix
    
    * Update frontend/assets/tailwind.css
    
    Co-authored-by: Ewelina Lasowy <56546832+EwelinaLasowy@users.noreply.github.com>
    
    * Changed import paths and scripts tags to match others
    
    * Lint fix
    
    Co-authored-by: Ewelina Lasowy <56546832+EwelinaLasowy@users.noreply.github.com>
    Co-authored-by: plencka <lencka.paulina@gmail.com>

--- frontend/components/NavigationBar.vue
+++ frontend/components/NavigationBar.vue
@@ -1,85 +1,132 @@
 <template>
-  <Disclosure as="nav" class="bg-primary" v-slot="{ open }">
-    <div class="max-w-7xl ml-2 mr-auto px-2 sm:px-6 lg:px-8 h-10">
-      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-        <DisclosureButton
-          class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-        >
-          <span class="sr-only">Open main menu</span>
-          <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-          <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
-        </DisclosureButton>
-      </div>
-      <div
-        class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start"
-      >
-        <div class="flex-shrink-0 flex items-center">
-          <img
-            class="lg:block h-10 w-auto"
-            src="../assets/images/navbar_logo.svg"
-          />
-        </div>
-        <div class="hidden sm:block sm:ml-6">
-          <div class="flex space-x-4">
-            <a
-              v-for="item in navigation"
-              :key="item.name"
-              :href="item.href"
-              :class="[
-                item.current
-                  ? 'bg-gray-900 text-white'
-                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                'px-3 py-2 rounded-md text-sm font-medium',
-              ]"
-              :aria-current="item.current ? 'page' : undefined"
-              >{{ item.name }}</a
-            >
-            <a
-              v-for="item in login"
-              :key="item.name"
-              :href="item.href"
-              :class="[
-                item.current
-                  ? 'bg-gray-900 text-white'
-                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                'px-3 py-2 rounded-md text-sm font-medium',
-              ]"
-              :aria-current="item.current ? 'page' : undefined"
-              >{{ item.name }}</a
-            >
+  <Disclosure as="nav" class="bg-primary z-50" v-slot="{ open }">
+    <div class="mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex items-center justify-between h-16">
+        <div class="flex items-center">
+          <div class="flex-shrink-0">
+            <img
+              class="block h-8 w-auto"
+              src="@/assets/images/navbar_logo.svg"
+              alt="Workflow"
+            />
           </div>
+          <div class="hidden sm:block sm:ml-6">
+            <div class="flex space-x-4">
+              <a
+                href="#"
+                class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                >Mapa</a
+              >
+              <a
+                href="#"
+                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >Dodaj firmę</a
+              >
+            </div>
+          </div>
+        </div>
+        <div class="hidden sm:ml-6 sm:block">
+          <div class="flex items-center">
+            <Menu as="div" class="ml-3 relative">
+              <div>
+                <MenuButton
+                  class="flex text-sm text-gray-50 rounded-full focus:outline-none focus:ring-2 focus:ring-gray-50"
+                >
+                  <div class="flex-shrink-0">
+                    <UserIcon class="h-6 w-6 rounded-full" />
+                  </div>
+                </MenuButton>
+              </div>
+
+              <transition
+                enter-active-class="transition ease-out duration-100"
+                enter-from-class="transform opacity-0 scale-95"
+                enter-to-class="transform opacity-100 scale-100"
+                leave-active-class="transition ease-in duration-75"
+                leave-from-class="transform opacity-100 scale-100"
+                leave-to-class="transform opacity-0 scale-95"
+              >
+                <MenuItems
+                  class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                >
+                  <MenuItem v-slot="{ active }">
+                    <a
+                      href="#"
+                      :class="[
+                        active ? 'bg-gray-100' : '',
+                        'block px-4 py-2 text-sm text-gray-700',
+                      ]"
+                      >Zaloguj</a
+                    >
+                  </MenuItem>
+                </MenuItems>
+              </transition>
+            </Menu>
+            <div class="flex items-center">
+              <div class="flex pl-5">
+                <a
+                  href="https://github.com/blumilksoftware/internships"
+                  class="bg-gray-800 p-1 rounded-full text-gray-400 focus:outline-none focus:ring-2 focus:ring-white"
+                >
+                  <span class="sr-only">View code</span>
+                  <CodeIcon class="h-6 w-6" aria-hidden="true" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="-mr-2 flex sm:hidden">
+          <DisclosureButton
+            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          >
+            <span class="sr-only">Open main menu</span>
+            <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
         </div>
       </div>
     </div>
-
     <DisclosurePanel class="sm:hidden">
       <div class="px-2 pt-2 pb-3 space-y-1">
         <DisclosureButton
-          v-for="item in navigation"
-          :key="item.name"
           as="a"
-          :href="item.href"
-          :class="[
-            item.current
-              ? 'bg-gray-900 text-white'
-              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-            'block px-3 py-2 rounded-md text-base font-medium',
-          ]"
-          :aria-current="item.current ? 'page' : undefined"
-          >{{ item.name }}</DisclosureButton
+          href="#"
+          class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+          >Mapa</DisclosureButton
         >
+        <DisclosureButton
+          as="a"
+          href="#"
+          class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+        >
+          Dodaj firmę
+        </DisclosureButton>
+      </div>
+      <div class="pt-4 pb-3 border-t border-gray-700">
+        <div class="flex items-center px-5"></div>
+        <div class="mt-3 px-2 space-y-1">
+          <DisclosureButton
+            as="a"
+            href="#"
+            class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
+          >
+            Zaloguj</DisclosureButton
+          >
+        </div>
       </div>
     </DisclosurePanel>
   </Disclosure>
 </template>
 
 <script setup>
-import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
-import { MenuIcon, XIcon } from "@heroicons/vue/outline";
-
-const navigation = [
-  { name: "Znajdz Praktyki", href: "#", current: true },
-  { name: "Dodaj swoją firmę", href: "#", current: false },
-  { name: "Logowanie", href: "#", current: false },
-];
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/vue";
+import { UserIcon, MenuIcon, XIcon, CodeIcon } from "@heroicons/vue/outline";
 </script>
--- frontend/views/app.blade.php
+++ frontend/views/app.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html class="h-full bg-blumilk-25">
+<html class="h-full bg-gray-100">
 <head>
+    <title>Praktyki @yield('title')</title>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
-    @vite('frontend/js/app.js')
+    @vite('js/app.js')
 </head>
-<body class="h-full">
-<div id="app"></div>
+<body class="h-full mt-0">
+    <div id="app" class="h-full"></div>
 </body>
 </html>
