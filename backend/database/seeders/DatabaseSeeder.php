089a22847ecdc565bcffd5f3ae8285096b829217
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2022-08-22 12:45:56 +0200

    Update user factory

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,10 +5,17 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Internships\Enums\Role;
+use Internships\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        User::factory(9)->create();
+        User::factory([
+            "role" => Role::Administrator,
+        ])
+            ->create();
     }
 }
