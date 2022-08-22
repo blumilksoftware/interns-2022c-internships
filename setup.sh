14b7a62ef337b39eff5e71c1bbb331cb0809109f
plencka <lencka.paulina@gmail.com>
2022-08-22 08:30:20 +0200

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -7,10 +7,10 @@ fi
 docker-compose build --no-cache --pull
 docker-compose up -d
 
-docker-compose exec node npm install
-docker-compose exec node npm run build
+docker-compose run --rm -u "$(id -u):$(id -g)" php composer install
+docker-compose run --rm -u "$(id -u):$(id -g)" php php artisan key:generate
 
-docker-compose exec php composer install
-docker-compose exec php php artisan key:generate
+docker-compose run --rm -u "$(id -u):$(id -g)" node npm install
+docker-compose run --rm -u "$(id -u):$(id -g)" node npm run build
 
 echo "After setting variables in .env file, remember to restart containers."
