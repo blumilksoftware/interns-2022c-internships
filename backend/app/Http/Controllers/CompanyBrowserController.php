d672b6a253c11d620c34e5981a3af3c77b0afdab
plencka <lencka.paulina@gmail.com>
2022-08-29 15:07:26 +0200

    Codestyle

--- backend/app/Http/Controllers/CompanyBrowserController.php
+++ backend/app/Http/Controllers/CompanyBrowserController.php
@@ -24,7 +24,7 @@ class CompanyBrowserController extends Controller
             [
                 "id" => 1,
                 "label" => "Bogatynia",
-            ]
+            ],
         ];
 
         return inertia(
