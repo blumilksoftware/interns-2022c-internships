f634a4598cd4620b5e443ad9f68491ffad6800b9
MatArasim <arasimmateusz@gmail.com>
2022-08-29 12:39:26 +0200

    Frontend browser header and search adjustment

--- frontend/js/Pages/CompanyBrowser/Components/CompanyListHeader.vue
+++ frontend/js/Pages/CompanyBrowser/Components/CompanyListHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="px-2 py-1 pt-4 pb-5 rounded-lg border-b bg-gray-50 border-gray-200"
+    class="px-2 py-1 pt-4 pb-5 rounded-lg border-b bg-gray-50 border-gray-200 hidden sm:block"
   >
     <h3 class="flex justify-center text-lg leading-6 font-medium text-gray-900">
       {{ $t("CompanyBrowser.CompaniesList") }}
@@ -9,30 +9,10 @@
       {{ $t("CompanyBrowser.FoundCompanies") }} : {{ companies.total }}
     </p>
   </div>
-  <div class="w-full">
-    <label for="search" class="sr-only"
-      >{{ $t("CompanyBrowser.Search") }}
-    </label>
-    <div class="relative">
-      <div
-        class="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center"
-      >
-        <SearchIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-      </div>
-      <input
-        id="search"
-        name="search"
-        class="block w-full bg-white border border-gray-300 rounded-md py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-slate-400 focus:border-slate-500 sm:text-sm"
-        :placeholder="$t('CompanyBrowser.Search')"
-        type="search"
-      />
-    </div>
-  </div>
+  
 </template>
 
 <script setup>
-import { SearchIcon } from "@heroicons/vue/solid";
-
 defineProps({
   companies: Object,
 });
