d631b2fd15d4170381635a686d4da5eb2d73bf68
plencka <lencka.paulina@gmail.com>
2022-09-02 13:48:40 +0200

    Fake logo generation for now

--- backend/app/Http/Requests/CompanyRequest.php
+++ backend/app/Http/Requests/CompanyRequest.php
@@ -6,6 +6,7 @@ namespace Internships\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Internships\Models\Embeddable\Coordinates;
+use Smknstd\FakerPicsumImages\FakerPicsumImagesProvider;
 
 class CompanyRequest extends FormRequest
 {
@@ -25,10 +26,17 @@ class CompanyRequest extends FormRequest
         $address = $this->get("address");
         $address["coordinates"] = $coordinates;
 
+        $logoName = null;
+        if(!$this->has("logo")) {
+            fake()->addProvider(new FakerPicsumImagesProvider(fake()));
+            $logoName = fake()->image(storage_path("app/public/images"), 200, 200, false);
+        }
+
         return [
             "name" => $this->get("name"),
             "description" => $this->get("description"),
             "user_id" => $this->get("user_id"),
+            "logo" => $logoName ?: $this->get("logo"),
             "address" => $address,
             "contact_details" => $this->get("contact_details"),
         ];
