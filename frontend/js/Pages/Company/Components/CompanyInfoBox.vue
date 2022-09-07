4a2cced24091750a625e747bea21b8753ef9e944
plencka <lencka.paulina@gmail.com>
2022-09-07 09:49:10 +0200

    SVG to HeroIcon

--- frontend/js/Pages/Company/Components/CompanyInfoBox.vue
+++ frontend/js/Pages/Company/Components/CompanyInfoBox.vue
@@ -1,6 +1,7 @@
 <script setup>
 import LocationIcon from "@/assets/icons/locationIcon.svg";
 import { onMounted } from "vue";
+import { XIcon } from "@heroicons/vue/outline";
 
 const props = defineProps({
   company: Object,
@@ -32,18 +33,7 @@ onMounted(() => {
   <div>
     <div class="sticky h-0 right-10 w-full flex justify-end">
       <button class="sticky right-0" @click="onClose">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="fill-current text-gray-700"
-          viewBox="0 0 16 16"
-          width="40"
-          height="40"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z"
-          ></path>
-        </svg>
+        <XIcon class="pr-5 pt-5 h-12 w-12 text-gray-700" />
       </button>
     </div>
     <div
