eff532012e40756150ffef9dc1c65e0f0e7f6224
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2022-12-06 18:42:05 +0100

    New company creation form

--- frontend/formkit.config.js
+++ frontend/formkit.config.js
@@ -6,19 +6,17 @@ export default {
   config: {
     classes: generateClasses({
       global: {
-        outer: "formkit-disabled:opacity-50",
-        messages: "list-none p-0 mt-1 mb-0",
+        outer: "formkit-disabled:opacity-50 uppercase text-xs font-semibold",
+        messages: "list-none p-0 mt-1 mb-0 ",
         message: "text-red-500 mb-1 text-xs",
         input:
-          "relative w-full my-2 max-w-lg w-screen ssm:w-80 shadow-sm focus:ring-primary focus:border-primary sm:text-sm border-gray-300 rounded-md formkit-invalid:border-red-500",
+          "w-full h-12 border-2 border-gray-300 rounded-xl shadow-sm font-normal",
       },
       submit: {
-        input:
-          "relative w-full m-y-2 w-60 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primary hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary disabled:opacity-20",
+        input: "",
       },
       button: {
-        input:
-          "relative w-full m-y-2 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-800 hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary",
+        input: "",
       },
     }),
   },
--- frontend/js/Pages/Company/Create.vue
+++ frontend/js/Pages/Company/Create.vue
@@ -1,289 +1,67 @@
 <script setup>
-import { useForm } from "@inertiajs/inertia-vue3";
-import InputError from "@/js/Shared/Components/InputError.vue";
-import { ref, watch } from "vue";
-import ImageUploader from "@/js/Shared/Components/ImageUploader.vue";
-import MarkdownEditor from "@/js/Shared/Components/MarkdownEditor.vue";
 import MapGeocode from "./Components/MapGeocode.vue";
+import MarkdownEditor from "@/js/Shared/Components/MarkdownEditor.vue";
 import Treeselect from "@tkmam1x/vue3-treeselect";
 import "@tkmam1x/vue3-treeselect/dist/vue3-treeselect.css";
-import { useI18n } from "vue-i18n";
-import { useToast } from "vue-toastification";
-import StepBar from "@/js/Shared/Components/StepBar.vue";
-import useSteps from "@/js/Shared/Components/useSteps.js";
-
-const i18n = useI18n();
-const toast = useToast();
-
-const props = defineProps({
-  departments: Object,
-});
-
-const form = useForm({
-  name: null,
-  description: "*" + i18n.t("add_company.description_info") + "*",
-  logoFile: null,
-  specializations: null,
-  address: {
-    street: null,
-    city: null,
-    postal_code: null,
-    voivodeship: null,
-    country: null,
-    coordinates: null,
-  },
-  contact_details: {
-    email: null,
-    website_url: null,
-    phone_number: null,
-  },
-});
-
-const specializationLimit = 4;
-let toastLimiter = false;
-
-function showSelectError() {
-  if (!toastLimiter) {
-    toast.error(i18n.t("status.cannot_select_more"));
-    toastLimiter = true;
-  } else {
-    return;
-  }
-  setTimeout(function () {
-    toastLimiter = false;
-  }, 3000);
-}
-
-watch(
-  () => form.specializations,
-  () => {
-    if (
-      form.specializations &&
-      form.specializations.length > specializationLimit
-    ) {
-      showSelectError();
-      form.specializations = form.specializations.slice(0, specializationLimit);
-    }
-  },
-  { deep: true }
-);
-
-function submit() {
-  form.address.coordinates = getCoordinatesFromMap();
-  form.post(route("company-store"), {
-    _method: "put",
-  });
-}
-
-let map = ref();
-
-function getLocationFromInputs() {
-  return [
-    form.address.street,
-    form.address.city,
-    form.address.voivodeship,
-    form.address.country,
-    form.address.postal_code,
-  ].join(", ");
-}
-
-function generateCoordinates() {
-  map.value.find(getLocationFromInputs());
-}
-
-function getCoordinatesFromMap() {
-  let coords = map.value.getCoordinates();
-
-  return {
-    latitude: coords.lat,
-    longitude: coords.lng,
-  };
-}
-
-const { steps, stepPlugin } = useSteps();
-
-const activeStep = ref("info");
-
-function onActiveStepChange(stepName) {
-  activeStep.value = stepName;
-}
 </script>
 <template>
-  <div class="w-full h-full">
-    <StepBar @stepChanged="onActiveStepChange" :steps="steps">
-      <template v-slot:content>
-        <FormKit
-          type="form"
-          #default="{ state: { valid } }"
-          :plugins="[stepPlugin]"
-          :actions="false"
-        >
-          <div
-            class="flex flex-col w-full h-full items-center justify-center p-8 gap-3 md:w-auto"
-          >
-            <section
-              class="flex flex-col items-center w-full ssm:w-screen"
-              v-show="activeStep === 'info'"
-            >
-              <label>{{ $t("add_company.logo") }}</label>
-              <ImageUploader class="h-52 w-52" v-model="form.logoFile" />
-              <InputError class="mt-2" :message="form.errors.logoFile" />
+  <div class="flex justify-center items-center gap-8 p-6 flex-col md:flex-row">
+    <div
+      class="border border-black aspect-[1/1] rounded-xl w-full max-w-[250px] ssm:w-80"
+    >
+      test
+    </div>
+    <div class="flex flex-col gap-3 w-full ssm:w-80">
+      <FormKit
+        type="text"
+        label="Nazwa firmy"
+        placeholder="Wprowadź nazwę firmy"
+      />
+      <FormKit
+        type="text"
+        label="E-mail firmowy"
+        placeholder="Wprowadź e-mail do kontaktu"
+      />
+      <FormKit
+        type="text"
+        label="Numer telefonu"
+        placeholder="Na przykład: 123-456-789"
+      />
+      <FormKit
+        type="text"
+        label="Strona internetowa"
+        placeholder="Na przykład: https://example.com"
+      />
+    </div>
+  </div>
 
-              <FormKit type="group" id="info" name="info">
-                <FormKit
-                  v-model="form.name"
-                  type="text"
-                  :label="$t('add_company.company_name')"
-                  validation="required|length:2,255"
-                />
-                <InputError :message="form.errors.name" />
+  <!-- sekcja 2 -->
+  <div
+    class="flex justify-center items-center gap-8 p-6 flex-col-reverse md:flex-row"
+  >
+    <div class="flex flex-col gap-3 w-full ssm:w-80">
+      <FormKit type="text" label="Kraj" />
+      <FormKit type="text" label="Województwo" />
+      <FormKit type="text" label="Miasto" />
+      <FormKit type="text" label="Kod pocztowy" />
+      <FormKit type="text" label="Ulica" />
+    </div>
+    <div class="border aspect-[1/1] rounded-xl w-full ssm:w-80">
+      <MapGeocode ref="map" class="rounded-lg" />
+    </div>
+  </div>
 
-                <FormKit
-                  v-model="form.contact_details.email"
-                  type="email"
-                  :label="$t('add_company.email')"
-                  validation="required|email"
-                />
-                <InputError :message="form.errors.contact_details" />
-
-                <FormKit
-                  v-model="form.contact_details.phone_number"
-                  :label="$t('add_company.phone_number')"
-                  type="text"
-                />
-
-                <FormKit
-                  v-model="form.contact_details.website_url"
-                  type="url"
-                  :label="$t('add_company.website_url')"
-                  validation="url"
-                />
-              </FormKit>
-            </section>
-
-            <section
-              class="flex flex-col items-center w-full ssm:w-screen"
-              v-show="activeStep === 'address'"
-            >
-              <FormKit type="group" id="address" name="address">
-                <div class="flex flex-col-reverse lg:flex-row">
-                  <div>
-                    <FormKit
-                      v-model="form.address.country"
-                      id="country"
-                      :label="$t('add_company.country')"
-                      type="text"
-                      validation="required"
-                    />
-
-                    <FormKit
-                      v-model="form.address.voivodeship"
-                      id="voivodeship"
-                      :label="$t('add_company.voivodeship')"
-                      type="text"
-                      validation="required"
-                    />
-
-                    <FormKit
-                      v-model="form.address.city"
-                      id="city"
-                      :label="$t('add_company.city')"
-                      type="text"
-                      validation="required"
-                    />
-
-                    <FormKit
-                      v-model="form.address.postal_code"
-                      :label="$t('add_company.postal_code')"
-                      id="postal_code"
-                      type="text"
-                      validation="required"
-                    />
-
-                    <FormKit
-                      v-model="form.address.street"
-                      :label="$t('add_company.street_address')"
-                      id="street"
-                      type="text"
-                      validation="required"
-                    />
-                    <InputError :message="form.errors.address" />
-
-                    <FormKit type="button" @click="generateCoordinates">
-                      {{ $t("add_company.generate_button") }}
-                    </FormKit>
-                  </div>
-                  <div class="pl-0 lg:pl-5 h-96 w-full lg:w-96">
-                    <MapGeocode ref="map" class="rounded-lg" />
-                  </div>
-                </div>
-              </FormKit>
-            </section>
-
-            <section
-              class="flex flex-col items-center w-screen ssm:w-full"
-              v-show="activeStep === 'description'"
-            >
-              <FormKit type="group" id="description" name="description">
-                <label>{{ $t("add_company.description") }}</label>
-                <div
-                  class="flex flex-col items-center justify-center lg:flex-row w-full h-full pt-2"
-                >
-                  <MarkdownEditor
-                    :preview="false"
-                    class="!h-64 md:!h-96 !w-full max-w-lg"
-                    v-model="form.description"
-                  />
-                  <MarkdownEditor
-                    :previewOnly="true"
-                    class="!h-64 md:!h-96 !w-full !p-5 max-w-lg"
-                    v-model="form.description"
-                  />
-                </div>
-                <FormKit
-                  type="hidden"
-                  v-model="form.description"
-                  validation="required"
-                />
-                <InputError :message="form.errors.description" />
-
-                <label class="pt-5">{{
-                  $t("add_company.select_specializations")
-                }}</label>
-                <Treeselect
-                  class="mt-2 max-w-md"
-                  :options="props.departments.data"
-                  :multiple="true"
-                  :disable-branch-nodes="true"
-                  :placeholder="$t('tree_selects.tree_select_specialization')"
-                  search-nested
-                  v-model="form.specializations"
-                />
-                <FormKit
-                  type="hidden"
-                  v-model="form.specializations"
-                  validation="required"
-                />
-              </FormKit>
-            </section>
-          </div>
-
-          <div
-            class="flex flex-row justify-center"
-            v-if="activeStep === 'description'"
-          >
-            <FormKit
-              type="submit"
-              @click="submit"
-              :disabled="form.processing || !valid"
-            >
-              {{ $t("buttons.request_button") }}
-            </FormKit>
-          </div>
-        </FormKit>
-      </template>
-    </StepBar>
+  <!-- sekcja 3 -->
+  <div class="flex justify-center items-center p-6 flex-col md:flex-row">
+    <MarkdownEditor
+      :preview="false"
+      class="!h-64 md:!h-96 !w-full max-w-lg"
+      v-model="test"
+    />
+    <MarkdownEditor
+      :previewOnly="true"
+      class="!h-64 md:!h-96 !w-full !p-5 max-w-lg"
+      v-model="test"
+    />
   </div>
 </template>
-
-<style></style>
